1a6075b30962924f27e2cbd9ada047c5c3e274b8
Irfan Abidin <irfan43abdin@gmail.com>
2022-06-29 22:51:59 +0700

    static no

--- pages/static/index.js
+++ pages/static/index.js
@@ -1,27 +1,27 @@
-import axios from "axios";
-import { useRouter } from "next/router";
-const StaticPage = ({ recipe }) => {
-  const router = useRouter();
-  return (
-    <div>
-      <h1>halaman static</h1>
-      <ul>
-        {recipe.map((item) => (
-          <li key={item.idrecipe} onClick={() => router.push(`/static/${item.id}`)}>
-            {item.title} - id: {item.idrecipe}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-export const getStaticProps = async () => {
-  const { data: RespData } = await axios.get(`http://localhost:4000/v1/recipe/`);
-  // const result = RespData.data;
-  // console.log(RespData.data);
-  return {
-    props: { recipe: RespData.data },
-  };
-};
+// import axios from "axios";
+// import { useRouter } from "next/router";
+// const StaticPage = ({ recipe }) => {
+//   const router = useRouter();
+//   return (
+//     <div>
+//       <h1>halaman static</h1>
+//       <ul>
+//         {recipe.map((item) => (
+//           <li key={item.idrecipe} onClick={() => router.push(`/static/${item.id}`)}>
+//             {item.title} - id: {item.idrecipe}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+// export const getStaticProps = async () => {
+//   const { data: RespData } = await axios.get(`http://localhost:4000/v1/recipe/`);
+//   // const result = RespData.data;
+//   // console.log(RespData.data);
+//   return {
+//     props: { recipe: RespData.data },
+//   };
+// };
 
-export default StaticPage;
+// export default StaticPage;
